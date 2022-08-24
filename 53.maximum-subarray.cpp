5dd28edba8b024318f742b699177f4631091c4f0
Mantispraying <srajanskgupta@gmail.com>
2022-08-24 12:28:05 +0530

    Solutions added

--- 53.maximum-subarray.cpp
+++ 53.maximum-subarray.cpp
@@ -12,7 +12,17 @@ class Solution
 public:
     int maxSubArray(vector<int> &nums)
     {
-        
+        int mxsum = INT_MIN, crsum = 0;
+        for (auto &x : nums)
+        {
+            crsum += x;
+            mxsum = max(mxsum, crsum);
+            if (crsum < 0)
+            {
+                crsum = 0;
+            }
+        }
+        return mxsum;
     }
 };
 // @lc code=end
