95a48b0eeefb1f7febe5ccd1b93fb618df6bc2cd
Mantispraying <srajanskgupta@gmail.com>
2022-09-03 23:53:53 +0530

    Solutions added

--- 70.climbing-stairs.cpp
+++ 70.climbing-stairs.cpp
@@ -10,6 +10,44 @@ using namespace std;
 class Solution
 {
 public:
+    vector<vector<int>> matrixMultiply(vector<vector<int>> &a, vector<vector<int>> &b)
+    {
+        vector<vector<int>> ans(a.size(), vector<int>(a.size()));
+
+        for (int i = 0; i < a.size(); i++)
+        {
+            for (int j = 0; j < a.size(); j++)
+            {
+                for (int k = 0; k < a.size(); k++)
+                {
+                    ans[i][j] += a[i][k] * b[k][j];
+                }
+            }
+        }
+        return ans;
+    }
+
+    vector<vector<int>> matrixExpo(vector<vector<int>> &a, int n)
+    {
+        if (n == 0)
+        {
+            return {{1, 0}, {0, 1}};
+        }
+        if (n == 1)
+        {
+            return a;
+        }
+
+        vector<vector<int>> temp = matrixExpo(a, n / 2);
+        temp = matrixMultiply(temp, temp);
+
+        if (n % 2)
+        {
+            return matrixMultiply(temp, a);
+        }
+        return temp;
+    }
+
     int count(int n, vector<int> &dp)
     {
         if (n < 3)
@@ -26,11 +64,19 @@ public:
 
     int climbStairs(int n)
     {
-        vector<int> dp(n + 1, -1);
-        return count(n, dp);
 
+        // matrix exponentiation
+        vector<vector<int>> a = {{1, 1}, {1, 0}};
+        vector<vector<int>> ans = matrixExpo(a, n);
+
+        return ans[0][0];
+
+        // Recursive Memoization
+        // vector<int> dp(n + 1, -1);
+        // return count(n, dp);
+
+        // Iterative tabulation
         // int a = 1, b = 1;
-
         // for (int i = 2; i <= n; i++)
         // {
         //     a = a + b;
