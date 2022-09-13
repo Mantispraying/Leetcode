346019b14eaaac00b4875115519057a1db0713d5
Mantispraying <srajanskgupta@gmail.com>
2022-09-13 20:46:57 +0530

    Solutions added

---
+++ 133.clone-graph.cpp
@@ -0,0 +1,79 @@
+#include <bits/stdc++.h>
+using namespace std;
+/*
+ * @lc app=leetcode id=133 lang=cpp
+ *
+ * [133] Clone Graph
+ */
+class Node
+{
+public:
+    int val;
+    vector<Node *> neighbors;
+    Node()
+    {
+        val = 0;
+        neighbors = vector<Node *>();
+    }
+    Node(int _val)
+    {
+        val = _val;
+        neighbors = vector<Node *>();
+    }
+    Node(int _val, vector<Node *> _neighbors)
+    {
+        val = _val;
+        neighbors = _neighbors;
+    }
+};
+// @lc code=start
+/*
+// Definition for a Node.
+class Node {
+public:
+    int val;
+    vector<Node*> neighbors;
+    Node() {
+        val = 0;
+        neighbors = vector<Node*>();
+    }
+    Node(int _val) {
+        val = _val;
+        neighbors = vector<Node*>();
+    }
+    Node(int _val, vector<Node*> _neighbors) {
+        val = _val;
+        neighbors = _neighbors;
+    }
+};
+*/
+
+class Solution
+{
+public:
+    Node *createNode(Node *node, unordered_map<Node *, Node *> &visited)
+    {
+        if (visited.find(node) == visited.end())
+        {
+            visited[node] = new Node(node->val, {});
+
+            for (auto &neighbor : node->neighbors)
+            {
+                visited[node]->neighbors.push_back(createNode(neighbor, visited));
+            }
+        }
+        return visited[node];
+    }
+
+    Node *cloneGraph(Node *node)
+    {
+        if (!node)
+            return node;
+
+        // unordered_map<Node *, Node *> visited;
+        // return createNode(node, visited);
+
+        
+    }
+};
+// @lc code=end
