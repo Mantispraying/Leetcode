5df65736313e95b3ba394f07cdf92e6927c0874e
Mantispraying <srajanskgupta@gmail.com>
2022-11-10 21:03:27 +0530

    Solutions added

--- Grind75/Week-4/238.product-of-array-except-self.cpp
+++ Grind75/Week-4/238.product-of-array-except-self.cpp
@@ -15,9 +15,9 @@ public:
         vector<int> ans(size(nums), 1);
         for (int i = 0, suf = 1, pre = 1, n = size(nums); i < n; i++)
         {
-            ans[i] *= pre;           // similar to prefix product being calculated in 1st loop of previous solution
+            ans[i] *= pre; // similar to prefix product being calculated in 1st loop of previous solution
             pre *= nums[i];
-            ans[n - 1 - i] *= suf;   // similar to suffix product being calculated in 2nd loop of previous solution
+            ans[n - 1 - i] *= suf; // similar to suffix product being calculated in 2nd loop of previous solution
             suf *= nums[n - 1 - i];
         }
         return ans;
