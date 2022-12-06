9d9b506ab3f915edc4962663d7ceba72f705e4e3
Mantispraying <srajanskgupta@gmail.com>
2022-12-06 13:31:59 +0530

    Solutions added

---
+++ Java/December2022/328.odd-even-linked-list.java
@@ -0,0 +1,51 @@
+package December2022;
+
+/*
+ * @lc app=leetcode id=328 lang=java
+ *
+ * [328] Odd Even Linked List
+ */
+class ListNode {
+    int val;
+    ListNode next;
+
+    ListNode() {
+    }
+
+    ListNode(int val) {
+        this.val = val;
+    }
+
+    ListNode(int val, ListNode next) {
+        this.val = val;
+        this.next = next;
+    }
+}
+
+// @lc code=start
+/**
+ * Definition for singly-linked list.
+ * public class ListNode {
+ * int val;
+ * ListNode next;
+ * ListNode() {}
+ * ListNode(int val) { this.val = val; }
+ * ListNode(int val, ListNode next) { this.val = val; this.next = next; }
+ * }
+ */
+class Solution {
+    public ListNode oddEvenList(ListNode head) {
+        if (head == null)
+            return head;
+        ListNode odd = head, even = head.next, evenStart = even;
+        while (even != null && even.next != null) {
+            odd.next = odd.next.next;
+            odd = odd.next;
+            even.next = even.next.next;
+            even = even.next;
+        }
+        odd.next = evenStart;
+        return head;
+    }
+}
+// @lc code=end
