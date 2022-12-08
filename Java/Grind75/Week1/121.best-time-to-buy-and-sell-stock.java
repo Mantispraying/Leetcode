e09cf569a6c2a53a5adc37c5b9acbeb78420d11a
Mantispraying <srajanskgupta@gmail.com>
2022-12-09 03:22:46 +0530

    Solutions added

--- Java/Grind75/Week1/121.best-time-to-buy-and-sell-stock.java
+++ Java/Grind75/Week1/121.best-time-to-buy-and-sell-stock.java
@@ -9,26 +9,13 @@ package Grind75.Week1;
 // @lc code=start
 class Solution {
     public int maxProfit(int[] prices) {
-        int maxProfit = 0;
-        int minPrice = Integer.MAX_VALUE;
-
-        // loop through all prices
+        int maxProfit = 0, lowestPrice = Integer.MAX_VALUE;
         for (int i = 0; i < prices.length; i++) {
-            // update the minimum price if necessary
-            if (prices[i] < minPrice) {
-                minPrice = prices[i];
-            }
-
-            // calculate the potential profit from buying at the minimum price
-            // and selling at the current price
-            int potentialProfit = prices[i] - minPrice;
-
-            // update the max profit if the potential profit is greater
-            if (potentialProfit > maxProfit) {
-                maxProfit = potentialProfit;
-            }
+            if (prices[i] < lowestPrice)
+                lowestPrice = prices[i];
+            else if (prices[i] - lowestPrice > maxProfit)
+                maxProfit = prices[i] - lowestPrice;
         }
-
         return maxProfit;
     }
 }
