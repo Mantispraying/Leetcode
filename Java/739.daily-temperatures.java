f5d1bbb0dd140d63791d973af42876f3a89853fc
Mantispraying <srajanskgupta@gmail.com>
2022-12-19 00:50:47 +0530

    Solutions added

---
+++ Java/739.daily-temperatures.java
@@ -0,0 +1,31 @@
+import java.util.*;
+/*
+ * @lc app=leetcode id=739 lang=java
+ *
+ * [739] Daily Temperatures
+ */
+
+// @lc code=start
+class Solution {
+    public int[] dailyTemperatures(int[] temperatures) {
+        Stack<Integer> stk = new Stack<>();
+        int[] daysToWarm = new int[temperatures.length];
+        for (int i = 0; i < temperatures.length; i++) {
+            while (!stk.empty() && temperatures[i] > temperatures[stk.peek()]) {
+                int idx = stk.pop();
+                daysToWarm[idx] = i - idx;
+            }
+            stk.push(i);
+        }
+        return daysToWarm;
+    }
+}
+
+// @lc code=end
+class Test {
+    public static void main(String[] args) {
+        Solution sol = new Solution();
+        int[] temperatures = { 73, 74, 75, 71, 69, 72, 76, 73 };
+        sol.dailyTemperatures(temperatures);
+    }
+}
