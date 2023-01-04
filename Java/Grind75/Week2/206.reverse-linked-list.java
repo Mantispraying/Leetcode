784f704857dbda96035207ec514f8a4d6b0dca6d
Mantispraying <srajanskgupta@gmail.com>
2023-01-04 22:29:16 +0530

    Solutions added

---
+++ Java/Grind75/Week2/206.reverse-linked-list.java
@@ -0,0 +1,48 @@
+package Grind75.Week2;
+
+/*
+ * @lc app=leetcode id=206 lang=java
+ *
+ * [206] Reverse Linked List
+ */
+class ListNode {
+    int val;
+    ListNode next;
+
+    ListNode() {
+    }
+
+    ListNode(int val) {
+        this.val = val;
+    }
+
+    ListNode(int val, ListNode next) {
+        this.val = val;
+        this.next = next;
+    }
+}
+
+// @lc code=start
+/**
+ * Definition for singly-linked list.
+ * public class ListNode {
+ * int val;
+ * ListNode next;
+ * ListNode() {}
+ * ListNode(int val) { this.val = val; }
+ * ListNode(int val, ListNode next) { this.val = val; this.next = next; }
+ * }
+ */
+class Solution {
+    public ListNode reverseList(ListNode head) {
+        ListNode prev = null;
+        while (head != null) {
+            ListNode next = head.next;
+            head.next = prev;
+            prev = head;
+            head = next;
+        }
+        return prev;
+    }
+}
+// @lc code=end
