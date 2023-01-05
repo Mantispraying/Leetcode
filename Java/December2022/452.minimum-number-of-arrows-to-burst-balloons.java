3839052771de3fc6b0c3e8c11b02ee5bef885641
Mantispraying <srajanskgupta@gmail.com>
2023-01-05 22:41:37 +0530

    Solutions added

---
+++ Java/December2022/452.minimum-number-of-arrows-to-burst-balloons.java
@@ -0,0 +1,24 @@
+package December2022;
+
+import java.util.*;
+/*
+ * @lc app=leetcode id=452 lang=java
+ *
+ * [452] Minimum Number of Arrows to Burst Balloons
+ */
+
+// @lc code=start
+class Solution {
+    public int findMinArrowShots(int[][] points) {
+        Arrays.sort(points, (a, b) -> Integer.compare(a[1], b[1]));
+        int ans = 0, pointer = 0;
+        for (int i = 0; i < points.length; i++) {
+            if (ans == 0 || points[i][0] > pointer) {
+                ans++;
+                pointer = points[i][1];
+            }
+        }
+        return ans;
+    }
+}
+// @lc code=end
