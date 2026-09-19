9200480728b0a7b83fb01e1fb10e740c0d38ea2c
Mantispraying <srajanskgupta@gmail.com>
2026-09-19 16:00:37 +0530

    Miscellaneous

---
+++ Java/Miscellaneous Problems/153.find-minimum-in-rotated-sorted-array.java
@@ -0,0 +1,24 @@
+import java.util.*;
+
+/*
+ * @lc app=leetcode id=153 lang=java
+ *
+ * [153] Find Minimum in Rotated Sorted Array
+ */
+
+// @lc code=start
+class Solution {
+    public int findMin(int[] nums) {
+        int l = 0, r = nums.length - 1;
+        while (l < r) {
+            int m = l + (r - l) / 2;
+            if (nums[m] < nums[r]) {
+                r = m;
+            } else {
+                l = m + 1;
+            }
+        }
+        return nums[l];
+    }
+}
+// @lc code=end
